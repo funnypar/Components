107743a555c9a79cc2b94f9121aecc6d4e3231b6
Mopano <99732517+funnypar@users.noreply.github.com>
2023-04-25 01:14:55 -0700

    addedItemsAndBtn

--- results-summary-component-main/tailwind.config.js
+++ results-summary-component-main/tailwind.config.js
@@ -8,8 +8,11 @@ module.exports = {
         extend: {
             colors: {
                 "Light-red": "hsl(0, 100%, 67%)",
+                "Bg-Light-red": "hsl(0, 100%, 98%)",
                 "Orangey-yellow": "hsl(39, 100%, 56%)",
+                "Bg-Orangey-yellow": "hsl(39, 100%, 98%)",
                 "Green-teal": "hsl(166, 100%, 37%)",
+                "Bg-Green-teal": "hsl(166, 100%, 98%)",
                 "Cobalt-blue": "hsl(234, 85%, 45%)",
                 "Pale-blue": "hsl(221, 100%, 96%)",
                 "Light-lavender": "hsl(241, 100%, 89%)",
@@ -19,6 +22,7 @@ module.exports = {
                 "Light-slate-blue": "hsl(252, 100%, 67%)",
                 "Light-royal-blue": "hsl(241, 81%, 54%)",
                 "Violet-blue": "hsla(256, 72%, 46%, 1)",
+                "Bg-Violet-blue": "hsl(256, 72%, 98%)",
                 "Persian-blue": "hsla(241, 72%, 46%, 0)",
             },
             width: {
