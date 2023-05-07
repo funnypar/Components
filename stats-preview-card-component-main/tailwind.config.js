321149c791550075e2aba08715237f8417932c90
Mopano <99732517+funnypar@users.noreply.github.com>
2023-05-07 01:44:34 -0700

    addedContents

--- stats-preview-card-component-main/tailwind.config.js
+++ stats-preview-card-component-main/tailwind.config.js
@@ -3,9 +3,19 @@ module.exports = {
     content: ["./**/*.{html,js}"],
     theme: {
         extend: {
+            colors: {
+                "Very-dark-blue": "hsl(233, 47%, 7%)",
+                "Dark-desaturated-blue": "hsl(244, 38%, 16%)",
+            },
             fontFamily: {
                 LexendDeca: ["Lexend Deca", "sans-serif"],
                 Inter: ["Inter", "sans-serif"],
+            },
+            height: {
+                "box-height": "35rem",
+            },
+            width: {
+                "box-width": "75rem",
             },
         },
     },
