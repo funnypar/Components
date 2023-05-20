9f522065863b2297c1e7473d37118749adedd3da
Mopano <99732517+funnypar@users.noreply.github.com>
2023-05-20 03:34:35 -0700

    finalDesktop

--- four-card-feature-section-master/tailwind.config.js
+++ four-card-feature-section-master/tailwind.config.js
@@ -7,10 +7,6 @@ module.exports = {
                 "Very-Dark-Blue": "hsl(234, 12%, 34%)",
                 "Grayish-Blue": "hsl(229, 6%, 66%)",
                 "Very-Light-Gray": "hsl(0, 0%, 98%)",
-                "my-Red": "hsl(0, 78%, 62%)",
-                "my-Cyan": "hsl(180, 62%, 55%)",
-                "my-Orange": "hsl(34, 97%, 64%)",
-                "my-Blue": "hsl(212, 86%, 64%)",
             },
             width: {
                 container: "35%",
