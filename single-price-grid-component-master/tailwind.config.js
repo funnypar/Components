641cdc226ffc6afb9b61b26ab918f12836bf12fe
Mopano <99732517+funnypar@users.noreply.github.com>
2023-05-22 08:44:54 -0700

    settingUp

---
+++ single-price-grid-component-master/tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./**/*.{html,js}"],
+    theme: {
+        extend: {
+            colors: {
+                "my-Cyan": "hsl(179, 62%, 43%)",
+                "Bright-Yellow": "hsl(71, 73%, 54%)",
+                "Light-Gray": "hsl(204, 43%, 93%)",
+                "Grayish-Blue": "hsl(218, 22%, 67%)",
+            },
+            screens: {
+                "mobile-size": "375px",
+            },
+        },
+    },
+    plugins: [],
+};
