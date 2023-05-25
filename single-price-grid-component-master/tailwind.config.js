28959b49cb5e24f385b499f887726ad8f16695a2
Mopano <99732517+funnypar@users.noreply.github.com>
2023-05-25 11:15:17 -0700

    finalDesktop

--- single-price-grid-component-master/tailwind.config.js
+++ single-price-grid-component-master/tailwind.config.js
@@ -5,12 +5,16 @@ module.exports = {
         extend: {
             colors: {
                 "my-Cyan": "hsl(179, 62%, 43%)",
+                "my-Cyan-Light": "hsl(179, 62%, 50%)",
                 "Bright-Yellow": "hsl(71, 73%, 54%)",
                 "Light-Gray": "hsl(204, 43%, 93%)",
                 "Grayish-Blue": "hsl(218, 22%, 67%)",
             },
             screens: {
                 "mobile-size": "375px",
+            },
+            width: {
+                "my-width": "68%",
             },
         },
     },
