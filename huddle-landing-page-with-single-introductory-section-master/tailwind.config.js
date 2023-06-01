3ad79f216a091d6a4a6139d4bed22c26d921eea4
Mopano <99732517+funnypar@users.noreply.github.com>
2023-06-01 13:00:10 -0700

    addedDesktop

--- huddle-landing-page-with-single-introductory-section-master/tailwind.config.js
+++ huddle-landing-page-with-single-introductory-section-master/tailwind.config.js
@@ -2,7 +2,24 @@
 module.exports = {
     content: ["./**/*.{html,js}"],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                Violet: "hsl(257, 40%, 49%)",
+                "Soft-Magenta": "hsl(300, 69%, 71%)",
+            },
+            width: {
+                logo: "20rem",
+            },
+            height: {
+                logo: "20rem",
+            },
+            fontFamily: {
+                heading: ["Poppins", "sans-serif"],
+            },
+            boxShadow: {
+                "3xl": "0 1rem 1rem .5rem rgba(0, 0, 0, 0.2)",
+            },
+        },
     },
     plugins: [],
 };
