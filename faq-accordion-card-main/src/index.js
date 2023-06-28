0bf4035a096b04aa2efd25763f4a39fdf201faee
Mopano <99732517+funnypar@users.noreply.github.com>
2023-06-28 04:22:57 -0700

    addedEachItemLogic

--- faq-accordion-card-main/src/index.js
+++ faq-accordion-card-main/src/index.js
@@ -1,5 +1,11 @@
 // Variables
 const btns = Array.from(document.querySelectorAll("[alt*='arrow']"));
 btns.forEach((el) => {
-    el.addEventListener("click", () => {});
+    el.addEventListener("click", () => {
+        const title = el.previousElementSibling;
+        const textElement = el.parentElement.parentElement.querySelector("h6");
+        el.classList.toggle("rotate-180");
+        title.classList.toggle("active");
+        textElement.classList.toggle("hidden");
+    });
 });
